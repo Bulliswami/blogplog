02a810e3c87269859a49d34a75406f01d994db7c
bulliswami <bulliswami.goluguri@gmail.com>
2022-04-30 19:56:31 +0530

    new form creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,13 @@ class ArticlesController < ApplicationController
         #binding.break
         @article=Article.find(params[:id])
     end
-    
+
     def index
         @articles=Article.all
+    end
+    def new
+    end
+    def create
+        render plain: params[:article]
     end
 end
